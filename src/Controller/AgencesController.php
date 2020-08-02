d0222771ad040e781e7c972e6df6179f7d0a6122
InFinity_54 <corentinkreicher@gmail.com>
2020-08-02 22:30:41 +0200

    Correction de bugs et d'erreurs + ajout de pages manquantes + masque de lien vers pages inexistantes du mockup

--- src/Controller/AgencesController.php
+++ src/Controller/AgencesController.php
@@ -21,4 +21,38 @@ class AgencesController extends AbstractController
     {
         return $this->render('content/agences/add.html.twig', []);
     }
+
+    /**
+     * @Route("/agences/add/submit", name="agences_add_submit")
+     */
+    public function agencesAddSubmit()
+    {
+        $this->addFlash("success", "L'agence a bien été ajoutée.");
+        return $this->redirectToRoute("agences_view");
+    }
+
+    /**
+     * @Route("/agences/view", name="agences_view")
+     */
+    public function agencesView()
+    {
+        return $this->render('content/agences/view.html.twig', []);
+    }
+
+    /**
+     * @Route("/agences/edit", name="agences_edit")
+     */
+    public function agencesEdit()
+    {
+        return $this->render('content/agences/edit.html.twig', []);
+    }
+
+    /**
+     * @Route("/agences/edit/submit", name="agences_edit_submit")
+     */
+    public function agencesEditSubmit()
+    {
+        $this->addFlash("success", "L'agence a bien été modifiée.");
+        return $this->redirectToRoute("agences_view");
+    }
 }
