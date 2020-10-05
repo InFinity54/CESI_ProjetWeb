2193bb78eb20f82e60da99e8e297b3852fcb0401
Axel SCHLOSSER <aschlosser@adista.fr>
2020-10-05 14:06:28 +0200

    Développement du back de la gestion des statuts

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Status;
 
 class StatusController extends AbstractController
 {
@@ -12,33 +14,75 @@ class StatusController extends AbstractController
      */
     public function statusList()
     {
-        return $this->render('content/status/index.html.twig', []);
+        if (!$this->getUser())
+        {
+            $this->addFlash("warning", "Vous n'êtes pas authentifié.");
+            return $this->redirectToRoute("login");
+        }
+        $status = $this->getDoctrine()->getRepository(Status::class)->findAll();
+        return $this->render('content/status/index.html.twig', [
+            "allstatus" => $status
+        ]);
     }
 
     /**
-     * @Route("/status/remove", name="status_remove")
+     * @Route("/status/remove/{id}", name="status_remove")
      */
-    public function statusRemove()
+    public function statusRemove(int $id)
     {
-        $this->addFlash("success", "Le statut a bien été supprimé.");
-        return $this->redirectToRoute("status");
+        if (!$this->getUser())
+        {
+            $this->addFlash("warning", "Vous n'êtes pas authentifié.");
+            return $this->redirectToRoute("login");
+        }
+
+        if ($this->getDoctrine()->getRepository(Status::class)->find($id))
+        {
+            $this->getDoctrine()->getManager()->remove($this->getDoctrine()->getRepository(Status::class)->find($id));
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash("success", "Le statut a bien été supprimé.");
+            return $this->redirectToRoute("status");
+        }
+        else
+        {
+            $this->addFlash("danger", "Le statut n'existe plus.");
+            return $this->redirectToRoute("status");
+        }
     }
 
-    
+
     /**
      * @Route("/status/add", name="status_add")
      */
     public function statusAdd()
     {
+        if (!$this->getUser())
+        {
+            $this->addFlash("warning", "Vous n'êtes pas authentifié.");
+            return $this->redirectToRoute("login");
+        }
         return $this->render('content/status/add.html.twig', []);
     }
 
     /**
      * @Route("/status/add/submit", name="status_add_submit")
      */
-    public function statusAddSubmit()
+    public function statusAddSubmit(Request $request)
     {
+        if (!$this->getUser())
+        {
+            $this->addFlash("warning", "Vous n'êtes pas authentifié.");
+            return $this->redirectToRoute("login");
+        }
+
+        $status = new Status();
+        $status->setName($request->request->get("nom"));
+        $status->setColor($request->request->get("couleur"));
+        $this->getDoctrine()->getManager()->persist($status);
+        $this->getDoctrine()->getManager()->flush();
+
         $this->addFlash("success", "Le statut a bien été ajouté.");
         return $this->redirectToRoute("status");
     }
 }
+
