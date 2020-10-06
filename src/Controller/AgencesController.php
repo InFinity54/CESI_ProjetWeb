38523b91aaba54da468813eae012ce3e75592c72
InFinity_54 <corentinkreicher@gmail.com>
2020-10-06 15:58:53 +0200

    Gestion des images des agences

--- src/Controller/AgencesController.php
+++ src/Controller/AgencesController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Service\AgencePictureUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,10 +29,9 @@ class AgencesController extends AbstractController
     /**
      * @Route("/agences/add/submit", name="agences_add_submit")
      */
-    public function agencesAddSubmit(Request $request)
+    public function agencesAddSubmit(Request $request, AgencePictureUploader $imageUploader)
     {
         $agence = new Agence();
-        //$agence->setImageAg($request->request->get('photo'));
         $agence->setImageAg('salut');
         $agence->setNomAg($request->request->get('nom'));
         $agence->setAdresseAg($request->request->get('adresse'));
@@ -40,6 +40,20 @@ class AgencesController extends AbstractController
         $agence->setVilleAg($request->request->get('ville'));
         $agence->setNumero($request->request->get('tel'));
         $agence->setFaxAg($request->request->get('fax'));
+
+        $this->getdoctrine()->getmanager()->persist($agence);
+        $this->getdoctrine()->getmanager()->flush();
+
+        $agenceimage = $imageUploader->upload($request->files->get("photo"), $agence->getId());
+
+        if ($agenceimage)
+        {
+            $agence->setImageAg($agenceimage);
+        }
+        else
+        {
+            $this->addFlash("warning", "Une erreur est survenue durant l'envoi de la photo de l'agence.");
+        }
 
         $this->getdoctrine()->getmanager()->persist($agence);
         $this->getdoctrine()->getmanager()->flush();
@@ -71,11 +85,12 @@ class AgencesController extends AbstractController
     /**
      * @Route("/agences/edit/{id}/submit", name="agences_edit_submit")
      */
-    public function agencesEditSubmit(int $id, Request $request)
+    public function agencesEditSubmit(int $id, Request $request, AgencePictureUploader $imageUploader)
     {
         $agence = $this->getDoctrine()->getRepository(Agence::class)->find($id);
-        //$agence->setImageAg($request->request->get('photo'));
-        $agence->setImageAg('salut');
+        $agenceimage = $imageUploader->upload($request->files->get("photo"), $id);
+
+        $agence->setImageAg($agenceimage);
         $agence->setNomAg($request->request->get('nom'));
         $agence->setAdresseAg($request->request->get('adresse'));
         $agence->setComplementAg($request->request->get('adressecomp'));
@@ -83,6 +98,15 @@ class AgencesController extends AbstractController
         $agence->setVilleAg($request->request->get('ville'));
         $agence->setNumero($request->request->get('tel'));
         $agence->setFaxAg($request->request->get('fax'));
+
+        if ($agenceimage)
+        {
+            $agence->setImageAg($agenceimage);
+        }
+        else
+        {
+            $this->addFlash("warning", "Une erreur est survenue durant l'envoi de la photo de l'agence.");
+        }
 
         $this->getdoctrine()->getmanager()->persist($agence);
         $this->getdoctrine()->getmanager()->flush();
