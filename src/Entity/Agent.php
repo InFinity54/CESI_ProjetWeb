3cbe9476a6221f025231038cb5d06767053c4ce0
InFinity_54 <corentinkreicher@gmail.com>
2020-10-11 03:34:13 +0200

    API VGest

--- src/Entity/Agent.php
+++ src/Entity/Agent.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\AgentRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=AgentRepository::class)
+ * @ApiResource
  */
 class Agent implements UserInterface
 {
